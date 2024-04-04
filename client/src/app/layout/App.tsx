22e05632e943011139864b75720d85dea2512252
Florin Iordache <iordacheflorin3@yahoo.com>
2024-04-04 16:43:01 +0200

    add theme provider

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,15 +1,36 @@
+import { useCallback, useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
-import { Container, CssBaseline } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background:{
+        default: palleteType === "light" ? "#eaeaea" : "#121212"
+      }
+    },
+  });
+
+  const handleThemeChange = useCallback(() => {
+    setDarkMode(!darkMode);
+  }, [darkMode]);
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
         <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }

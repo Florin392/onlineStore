981754e49be71912e9fd0217fceae809b3e3581b
Florin Iordache <iordacheflorin3@yahoo.com>
2024-04-04 16:03:47 +0200

    create catalog page and product components

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,3 +1,36 @@
+import { useEffect, useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import { Product } from "../models/products";
+import { Typography } from "@mui/material";
+
 export default function App() {
-  return <h1>hello world</h1>;
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  function addProduct() {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length + 101,
+        name: "product" + (prevState.length + 1),
+        price: prevState.length * 100 + 100,
+        brand: "some brand",
+        description: "some descrp",
+        pictureUrl: "htpp://picsum.photo/200",
+      },
+    ]);
+  }
+
+  return (
+    <>
+      <Typography variant="h1"></Typography>
+
+      <Catalog products={products} addProduct={addProduct} />
+    </>
+  );
 }

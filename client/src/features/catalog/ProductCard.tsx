088da2e8c501dc21e0d24ccbdcc22ec2a596a74d
Florin Iordache <iordacheflorin3@yahoo.com>
2024-04-04 21:35:44 +0200

    add links for navigating between pages

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -10,6 +10,7 @@ import {
 } from "@mui/material";
 import { Product } from "../../app/models/products";
 import { currencyFormat } from "../../app/helpers/utils";
+import { Link } from "react-router-dom";
 interface Props {
   product: Product;
 }
@@ -47,7 +48,9 @@ export default function ProductCard({ product }: Props) {
       </CardContent>
       <CardActions>
         <Button size="small">Add to cart</Button>
-        <Button size="small">View</Button>
+        <Button component={Link} to={`/catalog/${product.id}`} size="small">
+          View
+        </Button>
       </CardActions>
     </Card>
   );

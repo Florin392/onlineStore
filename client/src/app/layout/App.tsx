b1611a0e5e47f0973c2bc037256bd666ccbb0af5
Florin Iordache <iordacheflorin3@yahoo.com>
2024-04-04 16:30:10 +0200

    reformat catalog component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,38 +1,14 @@
-import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/products";
 import Header from "./Header";
 import { Container, CssBaseline } from "@mui/material";
 
 export default function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  function addProduct() {
-    setProducts((prevState) => [
-      ...prevState,
-      {
-        id: prevState.length + 101,
-        name: "product" + (prevState.length + 1),
-        price: prevState.length * 100 + 100,
-        brand: "some brand",
-        description: "some descrp",
-        pictureUrl: "htpp://picsum.photo/200",
-      },
-    ]);
-  }
-
   return (
     <>
       <CssBaseline />
       <Header />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog />
       </Container>
     </>
   );
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,21 +1,19 @@
-import {
-  Button,
-} from "@mui/material";
 import { Product } from "../../app/models/products";
 import ProductList from "./ProductList";
+import { useState, useEffect } from "react";
 
-interface Props {
-  products: Product[];
-  addProduct: () => void;
-}
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
 
-export default function Catalog({ products, addProduct }: Props) {
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <>
       <ProductList products={products} />
-      <Button variant="contained" onClick={addProduct}>
-        Add product
-      </Button>
     </>
   );
 }

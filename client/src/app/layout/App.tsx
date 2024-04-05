368624c6eaa2484cd37cc77fcbb36ff404c9e9a8
Florin Iordache <iordacheflorin3@yahoo.com>
2024-04-05 11:00:49 +0200

    apply logic for toast errors

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,6 +7,8 @@ import {
   createTheme,
 } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -26,6 +28,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>

7df02084fe493c265e26d4ef4500f57603a750af
florin392 <iordacheflorin3@yahoo.com>
2024-04-05 18:47:42 +0200

    add allow credentials for client side

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -5,6 +5,7 @@ import { router } from "../router/Routes";
 const sleep = () => new Promise((resolve) => setTimeout(resolve, 300));
 
 axios.defaults.baseURL = "http://localhost:5000/api/";
+axios.defaults.withCredentials = true;
 
 // helper
 const responseBody = (response: AxiosResponse) => response.data;

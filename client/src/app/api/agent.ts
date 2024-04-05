da1db50b1dac9c66d84e03fc5aef9f4b39b59a6e
Florin Iordache <iordacheflorin3@yahoo.com>
2024-04-05 10:44:36 +0200

    add axios requests

---
+++ client/src/app/api/agent.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from "axios";
+
+axios.defaults.baseURL = "http://localhost:5000/api/";
+
+// helper
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string) => axios.get(url).then(responseBody),
+  post: (url: string, body: object) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: object) => axios.put(url, body).then(responseBody),
+  delete: (url: string) => axios.delete(url).then(responseBody),
+};
+
+const Catalog = {
+  list: () => requests.get("products"),
+  details: (id: number) => requests.get(`products/${id}`),
+};
+
+const Errors = {
+  get400Error: () => requests.get("errors/bad-request"),
+  get401Error: () => requests.get("errors/unauthorised"),
+  get404Error: () => requests.get("errors/not-found"),
+  get500Error: () => requests.get("errors/server-error"),
+  getValidationError: () => requests.get("errors/validation-error"),
+};
+
+const agent = {
+  Catalog,
+  Errors,
+};
+
+export default agent;

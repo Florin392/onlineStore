f25b9f54a8864309d9f2ce595fff99ebf380d905
Florin Iordache <iordacheflorin3@yahoo.com>
2024-04-05 11:30:55 +0200

    add not found page

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -13,6 +13,7 @@ import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/products";
 import { currencyFormat } from "../../app/helpers/utils";
 import agent from "../../app/api/agent";
+import NotFound from "../../app/errors/NotFound";
 
 export default function ProductDetails() {
   const { id } = useParams<{ id: string }>();
@@ -29,7 +30,7 @@ export default function ProductDetails() {
 
   if (loading) return <h3>Loading...</h3>;
 
-  if (!product) return <h3>Product not found</h3>;
+  if (!product) return <NotFound />;
 
   return (
     <Grid container spacing={6}>

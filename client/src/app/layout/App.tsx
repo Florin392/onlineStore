92a9b3da62eb0eb52b992d0a046530cc51a6b436
florin392 <iordacheflorin3@yahoo.com>
2024-04-06 21:31:34 +0200

    add get cookies

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Header from "./Header";
 import {
   Container,
@@ -9,8 +9,14 @@ import {
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../helpers/utils";
+import agent from "../api/agent";
+import LoadingPage from "./LoadingComponent";
 
 export default function App() {
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
   const [darkMode, setDarkMode] = useState(false);
   const palleteType = darkMode ? "dark" : "light";
   const theme = createTheme({
@@ -22,9 +28,23 @@ export default function App() {
     },
   });
 
+  useEffect(() => {
+    const buyerId = getCookie("buyerId");
+    if (buyerId) {
+      agent.Basket.get()
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket]);
+
   const handleThemeChange = useCallback(() => {
     setDarkMode(!darkMode);
   }, [darkMode]);
+
+  if (loading) return <LoadingPage message="Initialising app..." />;
 
   return (
     <ThemeProvider theme={theme}>

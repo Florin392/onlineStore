7ee42dcea92181985ab203a5dbf2e7400bb8f1f6
florin392 <iordacheflorin3@yahoo.com>
2024-04-09 11:09:41 +0200

    add sort method

---
+++ API/Extensions/ProductExtentions.cs
@@ -0,0 +1,21 @@
+using API.Entities;
+
+namespace API.Extensions
+{
+    public static class ProductExtentions
+    {
+        public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy)
+        {
+
+            if (string.IsNullOrWhiteSpace(orderBy)) return query.OrderBy(p => p.Name);
+
+            query = orderBy switch
+            {
+                "price" => query.OrderBy(p => p.Price),
+                "priceDesc" => query.OrderByDescending(p => p.Price),
+                _ => query.OrderBy(p => p.Name)
+            };
+            return query;
+        }
+    }
+}

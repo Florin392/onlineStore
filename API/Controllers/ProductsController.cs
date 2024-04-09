c4e8950f9177daf7de4c6c56f5183a338996651c
florin392 <iordacheflorin3@yahoo.com>
2024-04-09 11:22:17 +0200

    add filter method

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -17,11 +17,12 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm)
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm, string brands, string types)
         {
             var query = _context.Products
             .Sort(orderBy)
             .Search(searchTerm)
+            .Filter(brands, types)
             .AsQueryable();
 
             return await query.ToListAsync();
--- API/Extensions/ProductExtentions.cs
+++ API/Extensions/ProductExtentions.cs
@@ -26,5 +26,23 @@ namespace API.Extensions
 
             return query.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm));
         }
+
+        public static IQueryable<Product> Filter(this IQueryable<Product> query, string brands, string types)
+        {
+            var brandList = new List<string>();
+            var typeList = new List<string>();
+
+            if (!string.IsNullOrEmpty(brands))
+                brandList.AddRange(brands.ToLower().Split(",").ToList());
+            if (!string.IsNullOrEmpty(types))
+                typeList.AddRange(types.ToLower().Split(",").ToList());
+
+            query = query.Where(p => brandList.Count == 0 || brandList.Contains(p.Brand.ToLower()));
+            // retur a query -> look for all of the brands/types that match anything that's inside the brands/types list
+            query = query.Where(p => typeList.Count == 0 || typeList.Contains(p.Type.ToLower()));
+
+            return query;
+
+        }
     }
 }

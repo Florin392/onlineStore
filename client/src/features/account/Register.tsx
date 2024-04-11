820ad56b22033a813dff74f16752784078ad1619
florin392 <iordacheflorin3@yahoo.com>
2024-04-11 10:38:32 +0200

    add register page

---
+++ client/src/features/account/Register.tsx
@@ -0,0 +1,96 @@
+import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Paper } from "@mui/material";
+import { Link } from "react-router-dom";
+import { FieldValues, useForm } from "react-hook-form";
+import { useCallback } from "react";
+import { LoadingButton } from "@mui/lab";
+import agent from "../../app/api/agent";
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isValid, errors },
+  } = useForm({
+    mode: "onTouched",
+  });
+
+  const submitForm = useCallback(async (data: FieldValues) => {
+    agent.Account.register(data);
+  }, []);
+
+  return (
+    <Container
+      component={Paper}
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        p: 4,
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(submitForm)}
+        noValidate
+        sx={{ mt: 1 }}
+      >
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Username"
+          autoFocus
+          {...register("username", { required: "Username is required" })}
+          error={!!errors.username}
+          helperText={errors?.username?.message as string}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Email"
+          {...register("email", { required: "Email is required" })}
+          error={!!errors.email}
+          helperText={errors?.email?.message as string}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Password"
+          type="password"
+          {...register("password", { required: "Password is required" })}
+          error={!!errors.password}
+          helperText={errors?.password?.message as string}
+        />
+
+        <LoadingButton
+          loading={isSubmitting}
+          disabled={!isValid}
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Register
+        </LoadingButton>
+        <Grid container>
+          <Grid item>
+            <Link to="/login">{"Already have an account? Sign In"}</Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+}

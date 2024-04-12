3882d786c02d6980733ef69798545a3a32bcc0b7
florin392 <iordacheflorin3@yahoo.com>
2024-04-12 14:57:35 +0200

    add order migration

--- API/Entities/OrderAggregate/Order.cs
+++ API/Entities/OrderAggregate/Order.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities.OrderAggregate
 {
     public class Order
     {
         public int Id { get; set; }
         public string BuyerId { get; set; }
+
+        [Required]
         public ShippingAddress ShippingAddress { get; set; }
         public DateTime OrderDate { get; set; } = DateTime.UtcNow;
         public List<OrderItem> OrderItems { get; set; }

6e489eb8e7056b8f7dc8a9096e828bee395d5b5b
florin392 <iordacheflorin3@yahoo.com>
2024-04-12 17:11:39 +0200

    add addressform,validation schema

---
+++ client/src/features/checkout/AddressForm.tsx
@@ -0,0 +1,45 @@
+import { Typography, Grid } from "@mui/material";
+import AppTextInput from "../../app/components/AppTextInput";
+import { useFormContext } from "react-hook-form";
+import AppCheckbox from "../../app/components/AppChecbox";
+
+export default function AddressForm() {
+  const { control } = useFormContext();
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping address
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <AppTextInput control={control} name="fullName" label="Full name" />
+        </Grid>
+        <Grid item xs={12}>
+          <AppTextInput control={control} name="address1" label="Address 1" />
+        </Grid>
+        <Grid item xs={12}>
+          <AppTextInput control={control} name="address2" label="Address 2" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <AppTextInput control={control} name="city" label="City" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <AppTextInput control={control} name="state" label="State" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <AppTextInput control={control} name="zip" label="Zipcode" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <AppTextInput control={control} name="country" label="Country" />
+        </Grid>
+        <Grid item xs={12}>
+          <AppCheckbox
+            name="saveAddress"
+            label="Save this as default address"
+            control={control}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+}

ce22598827ef2bae43bf7d8846ac742d199dafde
florin392 <iordacheflorin3@yahoo.com>
2024-04-18 12:01:26 +0200

    add delete product endpoint

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -94,5 +94,21 @@ namespace API.Controllers
             return BadRequest(new ProblemDetails { Title = "Problem updating product" });
         }
 
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            _context.Products.Remove(product);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest(new ProblemDetails { Title = "Problem deleting product" });
+        }
     }
 }

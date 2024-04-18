2694c9df25e633b101462656c57a9187cbb9f9a3
florin392 <iordacheflorin3@yahoo.com>
2024-04-18 12:01:00 +0200

    add update product endpoint

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -77,5 +77,22 @@ namespace API.Controllers
             return BadRequest(new ProblemDetails { Title = "Problem creating new product" });
         }
 
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+        {
+            var product = await _context.Products.FindAsync(productDto.Id);
+
+            if (product == null) return NotFound();
+
+            _mapper.Map(productDto, product);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return NoContent();
+
+            return BadRequest(new ProblemDetails { Title = "Problem updating product" });
+        }
+
     }
 }
